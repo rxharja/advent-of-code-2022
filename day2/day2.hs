4dec7ad105a0bb2c2e128a3667ed43255d157757
Redon Xharja <redon.xharja@gmail.com>
2022-12-04 20:34:54 -0500

    parsed input

--- day2/day2.hs
+++ day2/day2.hs
@@ -1,7 +1,22 @@
 -- Day 2: https://adventofcode.com/2022/day/2
 module Main where
 
+wordsWhen :: (Char -> Bool) -> String -> [String]
+wordsWhen p s = case dropWhile p s of
+  "" -> []
+  s' -> w : wordsWhen p s''
+    where
+      (w, s'') = break p s'
+
+splitIntoPairs :: String -> (Char, Char)
+splitIntoPairs s = (head fst, head lst)
+  where
+    l = wordsWhen (== ' ') s
+    fst = head l
+    lst = last l
+
 main :: IO ()
 main = do
   input <- readFile "day2-input.txt"
-  print input
+  let pairs = (map splitIntoPairs . words) input
+  print pairs
