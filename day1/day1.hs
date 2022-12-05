a16042352b835e43b7674cf17e758f95cb82c612
Redon Xharja <redon.xharja@gmail.com>
2022-12-04 19:51:11 -0500

    made it easier to read

--- day1/day1.hs
+++ day1/day1.hs
@@ -2,15 +2,18 @@
 
 module Main where
 
-wordsWhen :: String -> [String]
-wordsWhen xs = go xs "" []
+splitOnTwoNewLines :: String -> [String]
+splitOnTwoNewLines xs = go xs "" []
   where
     go "" _ acc = acc
     go ('\n' : '\n' : xs) sub acc = go xs "" (acc ++ [sub])
     go (x : xs) sub acc = go xs (sub ++ [x]) acc
 
+sumCalories :: String -> Int
+sumCalories = sum . map read . words
+
 maxCalories :: String -> Int
-maxCalories = maximum . map (sum . map read . words) . wordsWhen
+maxCalories = maximum . map sumCalories . splitOnTwoNewLines
 
 main :: IO ()
 main = do
