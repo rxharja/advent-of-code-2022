98e2d78257685cb66847ef1b37e2c7d1acd3bee6
Redon Xharja <redon.xharja@gmail.com>
2022-12-04 19:49:17 -0500

    made it easier to read

--- day1/day1.hs
+++ day1/day1.hs
@@ -1,9 +1,4 @@
 -- Day 1: Calorie Counting
--- 1\n2\n3\n\n4\n5\n
--- ["1\n2\n3\n", "4\n5\n"]
--- [[1,2,3], [4,5]]
--- [6, 9]
--- 9
 
 module Main where
 
@@ -14,8 +9,10 @@ wordsWhen xs = go xs "" []
     go ('\n' : '\n' : xs) sub acc = go xs "" (acc ++ [sub])
     go (x : xs) sub acc = go xs (sub ++ [x]) acc
 
+maxCalories :: String -> Int
+maxCalories = maximum . map (sum . map read . words) . wordsWhen
+
 main :: IO ()
 main = do
   calories <- readFile "day1-input.txt"
-  let x = maximum . map (sum . map read . words) . wordsWhen $ calories
-  print x
+  print $ maxCalories calories
