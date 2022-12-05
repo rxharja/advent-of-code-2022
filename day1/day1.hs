8fff3bc898c796f6f1a894eab995e9a3c9d00fc4
Redon Xharja <redon.xharja@gmail.com>
2022-12-04 19:47:49 -0500

    completed part 1

--- day1/day1.hs
+++ day1/day1.hs
@@ -1,6 +1,21 @@
+-- Day 1: Calorie Counting
+-- 1\n2\n3\n\n4\n5\n
+-- ["1\n2\n3\n", "4\n5\n"]
+-- [[1,2,3], [4,5]]
+-- [6, 9]
+-- 9
+
 module Main where
 
--- day 1: Calorie Counting
+wordsWhen :: String -> [String]
+wordsWhen xs = go xs "" []
+  where
+    go "" _ acc = acc
+    go ('\n' : '\n' : xs) sub acc = go xs "" (acc ++ [sub])
+    go (x : xs) sub acc = go xs (sub ++ [x]) acc
 
 main :: IO ()
-main = undefined
+main = do
+  calories <- readFile "day1-input.txt"
+  let x = maximum . map (sum . map read . words) . wordsWhen $ calories
+  print x
